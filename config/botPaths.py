482ba55634ccbc5a0b5316fe52a3457d13ef0b90
Joshua Wlodarczyk <43334820+FocusedLoop@users.noreply.github.com>
2025-03-07 16:46:38 +1000

    Update botPaths.py

--- config/botPaths.py
+++ config/botPaths.py
@@ -1,7 +1,7 @@
 FILE_PATHS = {
-    "url_file": r"C:\Users\joshu\OneDrive\Desktop\temp_python_workspace\bot\scrappedFiles\buildURLS.txt",
-    "build_file": r"C:\Users\joshu\OneDrive\Desktop\temp_python_workspace\bot\scrappedFiles\pc_build_parts.json",
-    "chrome_path": r"C:\chromedriver-win64\chromedriver.exe", # File path for chrome driver
-    "chrome_data": r"C:\Users\Joshua\AppData\Local\Google\Chrome\User Data", # File path for loading in chrome profile, user extensions, user data, etc
-    "nordvpn_path": r"C:\Program Files\NordVPN"
+    "url_file": r"",
+    "build_file": r"",
+    "chrome_path": r"", # File path for chrome driver
+    "chrome_data": r"", # File path for loading in chrome profile, user extensions, user data, etc
+    "nordvpn_path": r""
 }
